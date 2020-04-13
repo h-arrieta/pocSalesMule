3de386ad7531f58e6b68e47600a22c097ce27fed
gio93 <jorgerm1993@gmail.com>
2020-04-13 18:59:55 +0200

    minor fix configuration androidX capacitor 2

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -43,49 +43,7 @@ export class CardComponent implements OnInit {
     console.log(this.displayCards);
     // tslint:disable-next-line: align
     this.username = this._shareService.getSharedData();
-    this.getOpps();
-
-    console.log('Initializing HomePage');
-
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermission().then( result => {
-      if (result.granted) {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration', 
-      (token: PushNotificationToken) => {
-        alert('Push registration success, token: ' + token.value);
-      }
-    );
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError', 
-      (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived', 
-      (notification: PushNotification) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed', 
-      (notification: PushNotificationActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
+    this.getOpps(); 
   }
 
   opportunities() {
@@ -105,8 +63,55 @@ export class CardComponent implements OnInit {
     .subscribe((post: Array<CardData>) => {
       this.messages = post;
       this.displayCards = [];
+      console.log("GET OPPORTUNITIES FROM SALESFORCE");
       this.opportunities();
+      console.log("SUBSCRBING PUSH NOTIFICATIONS API FIREBASE");
+      this.subscribePushNotifications();
     });
+  }
+
+  subscribePushNotifications(){
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then( result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', 
+      (token: PushNotificationToken) => {
+        console.log('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', 
+      (error: any) => {
+        console.log('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived', 
+      (notification: PushNotification) => {
+        console.log('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed', 
+      (notification: PushNotificationActionPerformed) => {
+        console.log('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 
   changeTreatStatus(id: string) {
